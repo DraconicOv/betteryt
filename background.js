181b7975db1707d8954c6747e46b34fa553f43f9
DraconicOv <95949486+DraconicOv@users.noreply.github.com>
2023-05-02 20:07:32 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     if (details.url.indexOf("chrome://") === -1 && details.url.includes('youtube.com') && !details.url.includes('&themeRefresh=1') && (!details.url.includes(oldPage.replace("&themeRefresh=1","").replace("https://www.youtube.com","") ))) {
         oldPage=details.url;
     
-        chrome.tabs.executeScript(details.tabId, { file: "scripts/content.js" }, function() {    
+        chrome.tabs.executeScript(details.tabId, { file: "content.js" }, function() {    
             console.log(oldPage);
         });
     }
